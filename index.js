afda5f0c7e879c8be2621d11f533c0f3b0dda670
Chetonix <chetonix@gmail.com>
2022-01-22 13:45:11 +0530

    add buttonAnimation function and complete our website

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ for (let i = 0; i < document.getElementsByClassName("drum").length; i++) {
       buttonInnerHTML = this.innerHTML;
 
       makeSound(buttonInnerHTML);
+
+      buttonAnimation(buttonInnerHTML);
     });
 }
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -47,4 +50,13 @@ function makeSound(key) {
       console.log(buttonInnerHTML);
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
